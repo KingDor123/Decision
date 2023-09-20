40842a461a9a7e1f61d537c183d10584f4b5c776
Dor The King <dortheking@Dors-MacBook-Pro.local>
2023-09-20 03:36:00 +0300

    ddd

--- script.js
+++ script.js
@@ -3,29 +3,36 @@ const btn3=document.querySelector(".button3");
 const btn4=document.querySelector(".button4");
 const ans = document.querySelector(".answer")
 const clear = document.querySelector(".clear")
-const tempclass = document.querySelector(".animetion")
+const anime = document.querySelector(".animetion")
+const preAnswer = document.querySelector(".preAnswer")
+
+
 
 btn2.addEventListener("click",()=>{
     let number=Math.floor(Math.random() * 2)+1;
     console.log(number);
     ans.innerHTML=number;
-    tempclass.classList.remove("animetion")
+    anime.classList.remove("animetion")
+    preAnswer.innerHTML= " Your answer is: "
 })
 btn3.addEventListener("click",()=>{
     let number=Math.floor(Math.random() * 3)+1;
     console.log(number);
     ans.innerHTML=number;
-    tempclass.classList.remove("animetion")
+    anime.classList.remove("animetion")
+    preAnswer.innerHTML= " Your answer is: "
 })
 btn4.addEventListener("click",()=>{
     let number=Math.floor(Math.random() * 4)+1;
     console.log(number);
     ans.innerHTML=number;
-    tempclass.classList.remove("animetion")
+    anime.classList.remove("animetion")
+    preAnswer.innerHTML= " Your answer is: "
 })
 clear.addEventListener("click",()=>{
     console.log();
     ans.innerHTML = " "
-    tempclass.classList.add("animetion")
+    anime.classList.add("animetion")
+    preAnswer.innerHTML = " "
     
 })
